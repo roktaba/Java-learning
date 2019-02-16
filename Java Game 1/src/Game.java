7858ad8a8fe15789b3205fa4e86c0b7064a13a9e
roktaba <r.oktaba@gmail.com>
2019-02-16 12:27:28 +0100

    game2

--- Java Game 1/src/Game.java
+++ Java Game 1/src/Game.java
@@ -1,6 +1,28 @@
-public class Game {
+import java.awt.*;
 
-    public static void main(String[] args) {
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+
+    private Thread thread;
+    private boolean running = false;
+
+    public Game(){
+        new Window(WIDTH, HEIGHT, "Java Game 1", this);
+    }
+
+    public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
 
     }
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
+
 }
